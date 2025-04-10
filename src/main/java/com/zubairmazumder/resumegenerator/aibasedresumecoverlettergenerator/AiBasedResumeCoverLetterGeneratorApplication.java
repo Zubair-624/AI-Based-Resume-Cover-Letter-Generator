2f212a1d2fa57adf3b109095cbad4e3be2bc39a4
Zubair Mazumder <zubairmazumder624@gmail.com>
2025-04-10 20:16:59 +0600

    init: Create UserProfile model with basic resume attributes

---
+++ src/main/java/com/zubairmazumder/resumegenerator/aibasedresumecoverlettergenerator/AiBasedResumeCoverLetterGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.zubairmazumder.resumegenerator.aibasedresumecoverlettergenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBasedResumeCoverLetterGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBasedResumeCoverLetterGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zubairmazumder/resumegenerator/aibasedresumecoverlettergenerator/AiBasedResumeCoverLetterGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zubairmazumder.resumegenerator.aibasedresumecoverlettergenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBasedResumeCoverLetterGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
